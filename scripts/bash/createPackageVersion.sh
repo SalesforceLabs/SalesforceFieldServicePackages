1b9d59315c12241f94e8f113327f11a3b8238050
orenyulzary <oren.yulzary@techsee.me>
2021-11-16 22:59:20 +0200

    updating scripts

--- scripts/bash/createPackageVersion.sh
+++ scripts/bash/createPackageVersion.sh
@@ -3,7 +3,7 @@
 echo " "
 echo "Creating a package version..."
 
-sfdx force:package:version:create -p 0Ho690000004CG4CAM -k 12345 -v asperii@admin.fssk -f config/project-scratch-def.json --json -w 40 --skipvalidation
+sfdx force:package:version:create -p 0Ho4W00000000FJSAY --installationkeybypass -v asperii@admin.fssk -f config/project-scratch-def.json --json -w 40 --skipvalidation
 
 # sh scripts/bash/createPackageVersion.sh
 
